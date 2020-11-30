c59db44c85a5701378c37bd1467684c04cf53793
devebrahim <sadeghiebrahim.dev@gmail.com>
2020-12-01 01:39:40 +0330

    create preview image and Simulation with the original version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SnappForget-Password';
+  showSite:number=1
+  
+  constructor() {setTimeout(() => {
+    this.showSite = 0;
+  }, 2500);}
+  
+ngOnInit() {}
+
 }
